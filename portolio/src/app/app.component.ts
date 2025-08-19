6118bf41d5a9af5fd040eb1f7d2febbfb3c2fd20
freiremat <urbsfreire@gmail.com>
2025-08-18 22:56:48 -0300

    tasks: - imported missing modules on app comp - set menu comp on app comp html

--- portolio/src/app/app.component.ts
+++ portolio/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FooterComponent, TranslateModule],
+  imports: [CommonModule, MenuComponent, FooterComponent, TranslateModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
