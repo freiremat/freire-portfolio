61211aa463ce614b117fc8132d9495e08bc3a975
freiremat <urbsfreire@gmail.com>
2025-08-19 13:50:51 -0300

    tasks: - implement language toggle on menu comp

--- portolio/src/app/menu/language-toggle/language-toggle.component.ts
+++ portolio/src/app/menu/language-toggle/language-toggle.component.ts
@@ -1,32 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { CascadeSelectModule } from 'primeng/cascadeselect';
+import { DropdownModule } from 'primeng/dropdown';
 import { ButtonModule } from 'primeng/button';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-language-toggle',
   standalone: true,
-  imports: [CommonModule, FormsModule, CascadeSelectModule, ButtonModule],
+  imports: [CommonModule, FormsModule, DropdownModule, ButtonModule],
   templateUrl: './language-toggle.component.html',
   styleUrls: ['./language-toggle.component.css']
 })
 export class LanguageToggleComponent implements OnInit {
 
-  countries: any[] | undefined;
+  languages = [
+    { label: 'English (US)', value: 'en', flag: 'us' },
+    { label: 'Português (BR)', value: 'pt', flag: 'br' }
+  ];
 
-  selectedCity: any;
+  selectedLanguage: string = 'en';
 
-  ngOnInit() {
-    this.countries = [
-      {
-        name: 'United States',
-        code: 'US',
-      },
-      {
-        name: 'Brazil',
-        code: 'BR',
-      },
-    ];
-  }
+  ngOnInit() { }
 }
