911704668014709326cd57ece17190d7ae4e9d7a
freiremat <urbsfreire@gmail.com>
2025-08-21 12:25:52 -0300

    tasks: -implement i18n variables - create description comp and set values - imported font family on styles.css

--- portolio/src/app/app.component.ts
+++ portolio/src/app/app.component.ts
@@ -3,16 +3,17 @@ import { Component } from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { FooterComponent } from './footer/footer.component';
 import { MenuComponent } from './menu/menu.component';
+import { DescriptionComponent } from './description/description.component';
+import { FeaturedProjectsComponent } from './featured-projects/featured-projects.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, MenuComponent, FooterComponent, TranslateModule],
+  imports: [CommonModule, MenuComponent, DescriptionComponent, FeaturedProjectsComponent, FooterComponent, TranslateModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'portolio';
 
   constructor(public translate: TranslateService) {
     translate.addLangs(['en', 'pt']);
---
+++ portolio/src/app/description/description.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-description',
+  standalone: true,
+  imports: [TranslateModule],
+  templateUrl: './description.component.html',
+})
+export class DescriptionComponent {
+
+}
